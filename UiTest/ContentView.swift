0ea02ca9bb00bc66b800fb03cdee19bb1000e61b
Timothy Kipkorir Serem <kipkorirserem3@gmail.com>
2021-06-27 13:42:23 +0300

    Initial Commit

---
+++ UiTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UiTest
+//
+//  Created by Timothy Serem on 27/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UiTest/UiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiTestApp.swift
+//  UiTest
+//
+//  Created by Timothy Serem on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct UiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
